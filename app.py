74f4e067f0f8c91e2f1f5a09796dc12c5231318e
rishav-026 <rishavkumar7034@gmail.com>
2025-03-23 14:23:58 +0530

    added the search button and css

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"  # Fixed the database URI
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
+app = Flask(__name__, static_folder='static')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
-    sno = db.Column(db.Integer, primary_key=True)  # Fixed typo in `Column` and `Integer`
+    sno = db.Column(db.Integer, primary_key=True) 
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
@@ -17,44 +16,44 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
     
-@app.route('/',methods=['GET','POST'])
-def hello_world():
-    if request.method=='POST':
-        title=request.form.get("title")
-        desc=request.form.get("desc")
-        todo = Todo(title=title , desc=desc)
-        db.session.add(todo)
-        db.session.commit()
-    allTodo=Todo.query.all()
-    return render_template('index.html',allTodo=allTodo)  # Ensure `index.html` exists in the templates folder
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        title = request.form.get("title")
+        desc = request.form.get("desc")
+        if title and desc:
+            todo = Todo(title=title, desc=desc)
+            db.session.add(todo)
+            db.session.commit()
+        return redirect('/')
 
-@app.route('/show')
-def products():
-    allTodo = Todo.query.all()
-    print(allTodo)
-    return 'this is a products page'
+    query = request.args.get("query", "").strip() if request.args.get("query") else None
+    if query:
+        allTodo = Todo.query.filter(Todo.title.ilike(f"%{query}%")).all()
+    else:
+        allTodo = Todo.query.all()
 
-@app.route('/update/<int:sno>',methods=['GET','POST'])
+    return render_template('index.html', allTodo=allTodo, search_query=query)
+
+
+@app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
-    if request.method=='POST':
-        title=request.form.get("title")
-        desc=request.form.get("desc")
-        todo = Todo.query.filter_by(sno=sno).first()
-        todo.title=title
-        todo.desc=desc
-        db.session.add(todo)
+    todo = Todo.query.get_or_404(sno)
+
+    if request.method == 'POST':
+        todo.title = request.form.get("title")
+        todo.desc = request.form.get("desc")
         db.session.commit()
         return redirect("/")
-    todo = Todo.query.filter_by(sno=sno).first()
-    return render_template('update.html',todo=todo)
+
+    return render_template('update.html', todo=todo)
 
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
+    todo = Todo.query.get_or_404(sno)
     db.session.delete(todo)
     db.session.commit()
     return redirect('/')
 
-    
 if __name__ == '__main__':
     app.run(debug=True)
